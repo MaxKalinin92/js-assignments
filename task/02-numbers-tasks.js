d5bef69b195efec1308a680fd358546cb0f21693
MaxKalinin92 <67596381+MaxKalinin92@users.noreply.github.com>
2020-08-26 19:08:15 +0300

    Update 02-numbers-tasks.js

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -205,7 +205,7 @@ function roundToPowerOfTen(num, pow) {
  */
 function isPrime(n) {
     for (let i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i == 0) {
+        if (n % i === 0) {
             return false;
         }
     };
